69c395a980fc9c13660b374d4b6dbffb98bc7382
dearian <dearian2706@gmail.com>
2024-06-24 21:06:10 +0300

    Add auth

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,54 +1,72 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 import { IoAdd } from "react-icons/io5";
 import { IconContext } from "react-icons";
-import {useState} from 'react'
-import AddSectionModal from '../AddSectionModal';
-import LoginModal from "../LoginModal"
-import { SectionsProvider, useSectionsContext } from '../SectionProvider';
-
+import { useState } from "react";
+import AddSectionModal from "../AddSectionModal";
+import LoginModal from "../LoginModal";
+import { useSectionsContext } from "../SectionProvider";
 
 function Header() {
-    
-    const [openLogin, setOpenLogin] = useState(false);
-    const [open, setOpen] = useState(false);
-    const { user } = useSectionsContext();
-    
-    const closeLoginModal = () => {
-        setOpenLogin(false);
-    }
+  const [openLogin, setOpenLogin] = useState(false);
+  const [openAdmin, setOpenAdmin] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { user } = useSectionsContext();
 
-    const closeModal = () => {
-        setOpen(false);
-    }
+  const closeLoginModal = () => {
+    setOpenLogin(false);
+  };
 
-    console.log(user)
+  const closeModal = () => {
+    setOpen(false);
+  };
 
-	return (
-		<div className="header">
-            <div className='logo'>
-			<h1>Історія України</h1>
-            </div>
-            
+  const logOut = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
-                <button className='add_button' onClick={() => setOpen(!open)}>
-                    <IconContext.Provider value={{className: "add_icon" }}>
-                <div>
-                    <IoAdd />
-                </div>
-                </IconContext.Provider>
-                </button>
-                <AddSectionModal closeModal={closeModal} open={open} />       
-                
-                <button className='log_buton' onClick={() => setOpenLogin(!openLogin)}>
-                Увійти   
-                </button>    
+  console.log(user);
 
-                <LoginModal closeLoginModal={closeLoginModal} openLogin={openLogin} />   
+  return (
+    <div className="header">
+      <div className="logo">
+        <h1>Історія України</h1>
+      </div>
+      {user?.status === "admin" && (
+        <>
+          <button className="add_button" onClick={() => setOpen(!open)}>
+            <IconContext.Provider value={{ className: "add_icon" }}>
+              <div>
+                <IoAdd />
+              </div>
+            </IconContext.Provider>
+          </button>
+          <AddSectionModal closeModal={closeModal} open={open} />
+        </>
+      )}
+      {user?.status === "admin" ? (
+        <button className="log_buton" onClick={() => setOpenAdmin(!openAdmin)}>
+          Адмін
+        </button>
+      ) : user ? (
+        <button className="log_buton" onClick={() => logOut()}>
+          Вийти
+        </button>
+      ) : (
+        <>
+          <button
+            className="log_buton"
+            onClick={() => setOpenLogin(!openLogin)}
+          >
+            Увійти
+          </button>
 
-		</div>
-	)
+          <LoginModal closeLoginModal={closeLoginModal} openLogin={openLogin} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Header;
-
--- src/component/LoginModal/index.tsx
+++ src/component/LoginModal/index.tsx
@@ -1,145 +1,179 @@
-import React, { useState } from 'react';
-import './style.css';
-import RegistrationModal from "../RegistrationModal"
-import { useSectionsContext } from '../SectionProvider';
+import React, { useState } from "react";
+import "./style.css";
+import RegistrationModal from "../RegistrationModal";
+import { useSectionsContext } from "../SectionProvider";
+import { axios } from "../../utils/axios";
+import { useNavigate } from "react-router-dom";
 
 const LogiModal = ({ openLogin, closeLoginModal }) => {
-  const { signIn } = useSectionsContext();
-  const [isRegister, setIsRegister] = useState(false)
+  // const { signIn } = useSectionsContext();
+  const [isRegister, setIsRegister] = useState(false);
 
   const closeModal = () => {
-    setIsRegister(false)
-    closeLoginModal()
-  }
-  
+    setIsRegister(false);
+    closeLoginModal();
+  };
+
+  const handleLogin = () => {};
 
   if (!openLogin) return null;
 
   return (
     <div className="modal-container" onClick={closeModal}>
       <div className="login_modal" onClick={(e) => e.stopPropagation()}>
-        {isRegister ? <RegisterForm setIsRegister={setIsRegister}/> : <LoginForm setIsRegister={setIsRegister} signIn={signIn} closeModal={closeModal}/>}
+        {isRegister ? (
+          <RegisterForm setIsRegister={setIsRegister} />
+        ) : (
+          <LoginForm setIsRegister={setIsRegister} closeModal={closeModal} />
+        )}
       </div>
     </div>
-   
   );
 };
 
-const LoginForm = ({ setIsRegister, signIn, closeModal }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+const LoginForm = ({ setIsRegister, closeModal }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const authData = {email, password}
-
-  const handleLogin = () => {
-    if(!email || !password) {
+  const handleLogin = async () => {
+    if (!email || !password) {
       alert("Всі поля є обов'язковими.");
       return;
     }
-
-    signIn(authData)
-    setEmail('')
-    setPassword('')
-    closeModal()
-  }
-
-  return (
-  <>
-    <h1>Увійти</h1>
-    <div className='inputs'>
-      <input
-        type="text"
-        placeholder='*Email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder='*Пароль'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-    </div>
-    <div className="swich_button">
-    <button onClick={handleLogin} className='accept_button'>
-      Підтвердити
-    </button>
-    <div>
-      <button className='reg_button' onClick={() => setIsRegister(true)} >Реєстрація</button>
-    </div>
-    </div>
-  </>
-  )
-}
-
-const RegisterForm = ({ setIsRegister }) => {
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
+    try {
+      const res = await axios.post("/auth/signin", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", res.data.token);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-       <h1>Реєстрація</h1>
-        <div className='inputs'>
-          <input
-            type="text"
-            placeholder='*Email'
-    
-            />
-
-          <input
-            type="text"
-            placeholder="*Ім`я"
-       
-            />
-
-        
-            
-          <input
-            type="text"
-            placeholder='*Пароль'
-            
-            />
-
-          </div>
-          <div className='swich_button'>
-        <button className='accept_button'>
+      <h1>Увійти</h1>
+      <div className="inputs">
+        <input
+          type="text"
+          placeholder="*Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="*Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="swich_button">
+        <button onClick={handleLogin} className="accept_button">
           Підтвердити
         </button>
-        <button className='reg_button' onClick={() => setIsRegister(false)}>Увійти</button>
+        <div>
+          <button className="reg_button" onClick={() => setIsRegister(true)}>
+            Реєстрація
+          </button>
         </div>
-      </>
-    )
-}
+      </div>
+    </>
+  );
+};
+
+const RegisterForm = ({ setIsRegister }) => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    if (!email || !password || !name) {
+      alert("Всі поля є обов'язковими.");
+      return;
+    }
+    try {
+      console.log(email, password, name);
+      const res = await axios.post("/auth/signup", {
+        email,
+        name,
+        password,
+      });
+      localStorage.setItem("token", res.data.token);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <h1>Реєстрація</h1>
+      <div className="inputs">
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="*Email"
+        />
+
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="*Ім`я"
+        />
+
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          placeholder="*Пароль"
+        />
+      </div>
+      <div className="swich_button">
+        <button onClick={() => handleRegister()} className="accept_button">
+          Підтвердити
+        </button>
+        <button
+          type="button"
+          className="reg_button"
+          onClick={() => setIsRegister(false)}
+        >
+          Увійти
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default LogiModal;
 
-    //  <div className="modal-container" onClick={closeLoginModal}>
-    //   <div className="login_modal" onClick={(e) => e.stopPropagation()}>
-    //     <h1>Увійти</h1>
-    //     <div className='inputs'>
-    //       <input
-    //         type="text"
-    //         placeholder='*Email'
-           
-    //       />
-    //       <input
-    //         type="text"
-    //         placeholder='*Пароль'
-           
-    //       />
-    //     </div>
-    //     <div className='swich_button'>
-    //     <button className='accept_button'>
-    //       Підтвердити
-    //     </button>
-    //    <div onClick={closeLoginModal}>
-    //     <button className='reg_button'  onClick={() => setOpenReg(!openReg)} >Реєстрація</button>
-    //     </div>
-    //     <RegistrationModal  closeRegModal={closeRegModal} openReg={openReg} />
-    //     </div>
-    // </div>
-        
+//  <div className="modal-container" onClick={closeLoginModal}>
+//   <div className="login_modal" onClick={(e) => e.stopPropagation()}>
+//     <h1>Увійти</h1>
+//     <div className='inputs'>
+//       <input
+//         type="text"
+//         placeholder='*Email'
 
+//       />
+//       <input
+//         type="text"
+//         placeholder='*Пароль'
 
-    //   </div>
+//       />
+//     </div>
+//     <div className='swich_button'>
+//     <button className='accept_button'>
+//       Підтвердити
+//     </button>
+//    <div onClick={closeLoginModal}>
+//     <button className='reg_button'  onClick={() => setOpenReg(!openReg)} >Реєстрація</button>
+//     </div>
+//     <RegistrationModal  closeRegModal={closeRegModal} openReg={openReg} />
+//     </div>
+// </div>
+
+//   </div>
